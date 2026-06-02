3aa04464f58ec3956c8c5b085b330e32bbba837e
MyProjects05 <lukazaridze60@gmail.com>
2026-06-03 01:41:20 +0400

    Added UI

--- javascripts.js
+++ javascripts.js
@@ -1,77 +1,145 @@
 function getComputerChoice(){
-    const computerChoice = ["rock", "paper", "scissors"];
+    const computerCoice = ["rock", "paper", "scissors"];
     let num = Math.floor(Math.random()*3);
-    return computerChoice[num]
+    return computerCoice[num];
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Rock, Paper or Scissors");
-    return humanChoice.toLowerCase();
-}
 
-let humanScore;
-let computerScore;
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice;
+const text = document.querySelector(".text")
+const my = document.querySelector(".my");
+const pc = document.querySelector(".pc")
+const img1 = document.createElement("img");
+const img2 = document.createElement("img");
 
 
 function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock"){
+        img1.src="rock.png";
+    }
+    else if (humanChoice === "paper"){
+        img1.src="paper.png"
+    }
+    else if (humanChoice === "scissors"){
+        img1.src="scissors.png"
+    }
+    if (computerChoice === "rock"){
+        img2.src="rock.png"
+    }
+    else if (computerChoice === "paper"){
+        img2.src="paper.png"
+    }
+    else if (computerChoice === "scissors"){
+        img2.src="scissors.png"
+    }
+    my.appendChild(img1);
+    pc.appendChild(img2);
     if (humanChoice === computerChoice){
-        console.log("TIE");
+        text.textContent = `TIE, Your score: ${humanScore}, Computer score: ${computerScore}`;
         return
     }
 
     if (humanChoice === "rock"){
         if (computerChoice === "scissors"){
             humanScore += 1;
-            console.log("YOU WIN");
+            if (humanScore === 5){
+                text.textContent = `YOU WIN THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+            text.textContent = `YOU WIN, Your score: ${humanScore}, Computer score: ${computerScore}`;
             return
         }
         computerScore += 1;
-        console.log("YOU LOSE");
+        if (computerScore === 5){
+                text.textContent = `YOU LOSE THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+        text.textContent = `YOU LOSE, Your score: ${humanScore}, Computer score: ${computerScore}`;
         return
     }
 
     if (humanChoice === "paper"){
         if (computerChoice === "rock"){
             humanScore += 1;
-            console.log("YOU WIN");
+            if (humanScore === 5){
+                text.textContent = `YOU WIN THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+            text.textContent = `YOU WIN, Your score: ${humanScore}, Computer score: ${computerScore}`;
             return
         }
         computerScore += 1
-        console.log("YOU LOSE");
+        if (computerScore === 5){
+                text.textContent = `YOU LOSE THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+        text.textContent = `YOU LOSE, Your score: ${humanScore}, Computer score: ${computerScore}`;
         return
     }
 
     if (humanChoice === "scissors"){
         if (computerChoice === "paper"){
             humanScore += 1
-            console.log("YOU WIN");;
+            if (humanScore === 5){
+                textContent = `YOU WIN THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+            text.textContent = `YOU WIN, Your score: ${humanScore}, Computer score: ${computerScore}`; 
             return;
         }
         computerScore += 1
-        console.log("YOU LOSE");
+        if (computerScore === 5){
+                textContent = `YOU LOSE THE GAME, Your score: ${humanScore}, Computer score: ${computerScore}`
+                humanScore = 0;
+                computerScore = 0;
+                return
+            }
+        text.textContent = `YOU LOSE, Your score: ${humanScore}, Computer score: ${computerScore}`;
         return
     }
 }
 
 
-function playGame(){
-    humanScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        let human = getHumanChoice();
-        let computer = getComputerChoice();
-        playRound(human, computer);
-        console.log(`Your score: ${humanScore}`);
-        console.log(`Computer score: ${computerScore}`);
-    }
 
-    if (humanScore > computerScore){
-        console.log("You win the game.");
-    }else if (humanScore < computerScore){
-        console.log("You lose the game");
-    }else{
-        console.log("TIE");
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+
+rock.addEventListener("click", getHumanChoice);
+paper.addEventListener("click", getHumanChoice);
+scissors.addEventListener("click", getHumanChoice);
+
+function getHumanChoice(event){
+    const name = event.target.id;
+    switch(name){
+        case "rock":
+            humanChoice = "rock";
+            break
+        case "scissors":
+            humanChoice = "scissors";
+            break
+        case "paper":
+            humanChoice = "paper";
+            break
     }
+    playRound(humanChoice, getComputerChoice());
 }
 
-playGame();
+
+
+
+
